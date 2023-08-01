021d188a99e5a9b04034989786d06bd2768555bb
Olga <helga_ser@mail.ru>
2023-08-01 21:46:52 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int bonus = 20; // кол-во рублей на 1 бонус
-        int stbilet = 15789; // стоимость билета
+        int price = 15789; // стоимость билета
 
-        System.out.println("Будет начислено " + stbilet / bonus + " бонусов");
+        System.out.println("Будет начислено " + price / bonus + " бонусов");
 
     }
 }
